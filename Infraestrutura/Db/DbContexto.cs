58d35ee357bdc9ec7a22de3fb37bbe2530ffcd74
Fernando Sousa <fjcs7@hotmail.com>
2025-10-17 11:41:55 -0300

    Fixing: Renomeando a classe DbContext para DbContexto para evitar conflitos com a classe de mesmo nome do EF,

---
+++ Infraestrutura/Db/DbContexto.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
+
+namespace MinimalApi.Infraestrutura.Db;
+
+public class DbContexto : Microsoft.EntityFrameworkCore.DbContext
+{
+    private readonly IConfiguration _configuracaoAppSettings;
+    public DbContexto(IConfiguration configuracaoAppSettings)
+    {
+        _configuracaoAppSettings = configuracaoAppSettings;
+    }
+    public DbSet<Administrador> Administradores { get; set; } = default;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Administrador>().HasData(
+            new Administrador
+            {
+                Id = 1,
+                Email = "administrador@teste.com",
+                Senha = "@123admin",
+                Perfil = "Adm"
+            }
+        );
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql")?.ToString();
+            if (!string.IsNullOrEmpty(stringConexao))
+            {
+                optionsBuilder.UseMySql(
+                    stringConexao,
+                    ServerVersion.AutoDetect(stringConexao)
+                    );
+            }
+        }
+    }
+}
