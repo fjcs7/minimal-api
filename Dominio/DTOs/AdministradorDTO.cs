ce39e9d9f960d7785c07e9cfd770dd0945f363aa
Fernando Sousa <fjcs7@hotmail.com>
2025-10-30 17:09:23 -0300

    Adicionando validação do range do Perfil de administrador.

--- Dominio/DTOs/AdministradorDTO.cs
+++ Dominio/DTOs/AdministradorDTO.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using MinimalApi.Dominio.Entidades;
 using MinimalApi.Dominio.Enuns;
 using MinimalApi.Dominio.ModelViews;
@@ -29,9 +30,42 @@ public record AdministradorDTO
         if (string.IsNullOrEmpty(this.Senha))
             validacao.Mensagens.Add("A senha não pode ser vazia.");
 
-        if (this.Perfil ==  null)
-            validacao.Mensagens.Add("A perfil não pode ser vazio.");
+        var validacaoPerfil = VerificaRangeDoPerfil();
+        if(!string.IsNullOrEmpty(validacaoPerfil))
+            validacao.Mensagens.Add(validacaoPerfil);
 
         return validacao;
+    }
+    
+    private string VerificaRangeDoPerfil()
+    {
+        if (this.Perfil == null)
+        {
+            return "A perfil não pode ser vazio.";
+        }
+
+        StringBuilder errosDeValidacao = new();
+        Enum.TryParse(typeof(Perfil), Perfil.ToString(), out var resultado);
+
+        if (!Enum.IsDefined(typeof(Perfil), resultado ?? -1))
+        {
+            errosDeValidacao.Append("Valor definido para o perfil fora do range aceito: ");
+            
+             int maiorValorNumerico = Enum.GetValues(typeof(Perfil))
+                                     .Cast<int>()
+                                     .Max();
+            foreach (int item in Enum.GetValues(typeof(Perfil)))
+            {
+                string espaco = string.Empty;
+                if (item < maiorValorNumerico)
+                    espaco = ", ";
+                else
+                    espaco = ".";
+
+                errosDeValidacao.Append($"'{item} - {(Perfil)(item)}'{espaco}");
+            }
+        }
+
+        return errosDeValidacao.ToString();
     }
 }
